7f8b9b0400c3a0d284f35f5e7e5fe576aa78f6ee
Omar <luizbr507z@gmail.com>
2020-10-28 19:42:43 +0100

    first commit

---
+++ src/API.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class API {
+    public void send()  {
+//        ServerSocket serverSocket = new ServerSocket(8081);
+
+        Socket socket = null;
+        try {
+            socket = new Socket("0.0.0.0", 8080);
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+            writer.println("test");
+
+            InputStream input = socket.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
